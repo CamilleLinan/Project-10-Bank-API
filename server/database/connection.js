3ed9eaa3e2cbe610e9dae299ca7bb07324e2319b
Camille Linan <camille.linan@hotmail.com>
2025-01-15 23:05:52 +0100

    corrections

--- server/database/connection.js
+++ server/database/connection.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const databaseUrl = "mongodb+srv://bZVq8QBa546g8p:MCo4sH7zZA41a4hd@argentbank.6qj0zst.mongodb.net/?retryWrites=true&w=majority&appName=ArgentBank"
+// const databaseUrl = process.env.DATABASE_URL
 
 module.exports = async () => {
   try {
